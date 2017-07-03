c59cbe2ecc20f30b78abe51fe0af21710f88fe57
langxingyong <langxingyong@163.com>
2017-07-03 15:53:36 +0800

    oko

--- 斗鱼直播/斗鱼直播/AppDelegate.swift
+++ 斗鱼直播/斗鱼直播/AppDelegate.swift
@@ -12,7 +12,8 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    // 修改
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
